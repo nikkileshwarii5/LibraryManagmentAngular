0215c889d9f8c9b4ae45d5cd5cb280136ea26645
DODLA NIKILESHWARI <143583926+nikkileshwarii5@users.noreply.github.com>
2026-07-30 15:49:19 +0530

    Update subscription.service.ts

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/subscription-plans';
+const API_URL = 'http://15.206.209.161:8080/api/subscription-plans';
 
 @Injectable({ providedIn: 'root' })
 export class SubscriptionService {
