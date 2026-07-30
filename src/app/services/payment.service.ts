59b978c628ad44feae4af90b109a0e9d8eeeb375
DODLA NIKILESHWARI <143583926+nikkileshwarii5@users.noreply.github.com>
2026-07-30 15:48:36 +0530

    Update payment.service.ts

--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const API = 'http://localhost:8080/api/payments';
+const API = 'http://15.206.209.161:8080/api/payments';
 
 @Injectable({ providedIn: 'root' })
 export class PaymentService {
