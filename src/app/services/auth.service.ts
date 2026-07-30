f7b676dfe1be2b64ee76218fda72b74f1f8247fa
DODLA NIKILESHWARI <143583926+nikkileshwarii5@users.noreply.github.com>
2026-07-30 15:45:48 +0530

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = 'http://15.206.209.161:8080';
 
 @Injectable({
   providedIn: 'root'
